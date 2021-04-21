e839af89f44f4ceba1ebd2d8b9dcffc50a27b45b
LinusTanYongWai <linustan.20@ichat.sp.edu.sg>
2021-04-21 12:48:15 +0800

    Make sum(x, y) use - operator

--- calculator.js
+++ calculator.js
@@ -11,10 +11,11 @@ module.exports.sum = (x, y) => {
 
 // https://www.geeksforgeeks.org/subtract-two-numbers-without-using-arithmetic-operators/
 module.exports.minus = (x, y) => {
-    while (y !== 0) {
-        let borrow = ~x & y;
-        x = x ^ y;
-        y = borrow << 1;
-    }
-    return x;
+    // while (y !== 0) {
+    //     let borrow = ~x & y;
+    //     x = x ^ y;
+    //     y = borrow << 1;
+    // }
+    // return x;
+    return x - y;
 };
